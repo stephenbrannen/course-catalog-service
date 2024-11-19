b9bc7ffb82b8db593c4d4efa2443afaae7538fc2
Stephen Brannen <sbrannen@atlassian.com>
2024-11-18 22:49:09 -0600

    Implement postgres db

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,14 +23,17 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.micrometer:micrometer-tracing-bridge-brave")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
 	//logging
 	implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
 
-	runtimeOnly("com.h2database:h2")
-	// runtimeOnly("org.postgresql:postgresql")
+	//database
+	//runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
